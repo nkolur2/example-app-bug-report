5547c2e4765ef7ff0c1d1040ae1dde59cd87249c
nkolur2 <106771860+nkolur2@users.noreply.github.com>
2022-11-21 00:35:52 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Vitamin Shoppe Demo", page_icon="🏀", layout="centered")
+st.set_page_config(page_title="Cerebro Roadmap", page_icon="🏀", layout="centered")
 
 from utils import gui
 
