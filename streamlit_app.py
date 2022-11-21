9e9196851cfdebbbec8cc80840105416171bed37
nkolur2 <106771860+nkolur2@users.noreply.github.com>
2022-11-21 00:03:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,101 +1,27 @@
-import google_auth_httplib2
-import httplib2
-import pandas as pd
 import streamlit as st
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from googleapiclient.http import HttpRequest
 
-SCOPE = "https://www.googleapis.com/auth/spreadsheets"
-SPREADSHEET_ID = "1_ATW0BcaUaT5aEPr2RsZMpgpye3THF6yGY_l9XUctZM"
-SHEET_NAME = "Database"
-GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
+st.set_page_config(page_title="Vitamin Shoppe Demo", page_icon="🌀", layout="centered")
 
+from utils import gui
 
-@st.experimental_singleton()
-def connect_to_gsheet():
-    # Create a connection object.
-    credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
-        scopes=[SCOPE],
-    )
+# gui.icon("🌀")
 
-    # Create a new Http() object for every request
-    def build_request(http, *args, **kwargs):
-        new_http = google_auth_httplib2.AuthorizedHttp(
-            credentials, http=httplib2.Http()
-        )
-        return HttpRequest(new_http, *args, **kwargs)
+st.image(
+    "https://www.newhope.com/sites/newhope360.com/files/vitaminshoppe-logo-2018-promo.png",
+    width=300, # Manually Adjust the width of the image as per requirement
+   )
 
-    authorized_http = google_auth_httplib2.AuthorizedHttp(
-        credentials, http=httplib2.Http()
-    )
-    service = build(
-        "sheets",
-        "v4",
-        requestBuilder=build_request,
-        http=authorized_http,
-    )
-    gsheet_connector = service.spreadsheets()
-    return gsheet_connector
+# Make sure session state is preserved
+for key in st.session_state:
+    st.session_state[key] = st.session_state[key]
 
-
-def get_data(gsheet_connector) -> pd.DataFrame:
-    values = (
-        gsheet_connector.values()
-        .get(
-            spreadsheetId=SPREADSHEET_ID,
-            range=f"{SHEET_NAME}!A:E",
-        )
-        .execute()
-    )
-
-    df = pd.DataFrame(values["values"])
-    df.columns = df.iloc[0]
-    df = df[1:]
-    return df
-
-
-def add_row_to_gsheet(gsheet_connector, row) -> None:
-    gsheet_connector.values().append(
-        spreadsheetId=SPREADSHEET_ID,
-        range=f"{SHEET_NAME}!A:E",
-        body=dict(values=row),
-        valueInputOption="USER_ENTERED",
-    ).execute()
-
-
-st.set_page_config(page_title="Bug report", page_icon="🐞", layout="centered")
-
-st.title("🐞 Bug report!")
-
-gsheet_connector = connect_to_gsheet()
-
-
-form = st.form(key="annotation")
-
-with form:
-    cols = st.columns((1, 1))
-    author = cols[0].text_input("Report author:")
-    bug_type = cols[1].selectbox(
-        "Bug type:", ["Front-end", "Back-end", "Data related", "404"], index=2
-    )
-    comment = st.text_area("Comment:")
-    cols = st.columns(2)
-    date = cols[0].date_input("Bug date occurrence:")
-    bug_severity = cols[1].slider("Bug severity:", 1, 5, 2)
-    submitted = st.form_submit_button(label="Submit")
-
-
-if submitted:
-    add_row_to_gsheet(
-        gsheet_connector,
-        [[author, bug_type, comment, str(date), bug_severity]],
-    )
-    st.success("Thanks! Your bug was recorded.")
-    st.balloons()
-
-expander = st.expander("See all records")
-with expander:
-    st.write(f"Open original [Google Sheet]({GSHEET_URL})")
-    st.dataframe(get_data(gsheet_connector))
+st.title("Welcome to the VS Customer Insights app!")
+st.sidebar.text(f"Account: {st.secrets.sf_usage_app.account}")
+st.sidebar.info("Choose a page!")
+st.markdown(
+    """
+This app provides insights on a Vitamin Shoppe's customers
+### Get started!
+👈 Select a page in the sidebar!
+    """
+)
