6a210cce0b4fba0953a1d0a3516965f2043de562
nkolur2 <106771860+nkolur2@users.noreply.github.com>
2022-11-21 00:35:11 -0500

    Create Roadmap.py

---
+++ pages/Roadmap.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(page_title="Cerebro Roadmap", page_icon="🏀", layout="centered")
+
+from utils import gui
+
+# gui.icon("🏀")
+
+st.image(
+    "https://s3.amazonaws.com/appforest_uf/f1645050160993x340144886538052540/Cerebro%20Logo%20Black-J8AG.png",
+    width=300, # Manually Adjust the width of the image as per requirement
+   )
+
+# Make sure session state is preserved
+for key in st.session_state:
+    st.session_state[key] = st.session_state[key]
+
+st.title("Page in Progress")
