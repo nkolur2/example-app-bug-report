1b0f50d8b2b06e62fe45a985fd1d6f90aac71bd3
nkolur2 <106771860+nkolur2@users.noreply.github.com>
2022-12-06 17:55:14 -0500

    Update Roadmap.py

--- pages/Roadmap.py
+++ pages/Roadmap.py
@@ -6,10 +6,24 @@ from utils import gui
 
 # gui.icon("🏀")
 
-st.image(
-    "https://s3.amazonaws.com/appforest_uf/f1645050160993x340144886538052540/Cerebro%20Logo%20Black-J8AG.png",
-    width=300, # Manually Adjust the width of the image as per requirement
-   )
+import streamlit as st
+from streamlit_timeline import st_timeline
+
+st.set_page_config(layout="wide")
+
+items = [
+    {"id": 1, "content": "2022-10-20", "start": "2022-10-20"},
+    {"id": 2, "content": "2022-10-09", "start": "2022-10-09"},
+    {"id": 3, "content": "2022-10-18", "start": "2022-10-18"},
+    {"id": 4, "content": "2022-10-16", "start": "2022-10-16"},
+    {"id": 5, "content": "2022-10-25", "start": "2022-10-25"},
+    {"id": 6, "content": "2022-10-27", "start": "2022-10-27"},
+]
+
+timeline = st_timeline(items, groups=[], options={}, height="300px")
+st.subheader("Selected item")
+st.write(timeline)
+
 
 # Make sure session state is preserved
 for key in st.session_state:
