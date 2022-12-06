3faaf223baf6d7a307a5fe72b35c0c7391cb8615
nkolur2 <106771860+nkolur2@users.noreply.github.com>
2022-12-06 18:20:51 -0500

    Update Roadmap.py

--- pages/Roadmap.py
+++ pages/Roadmap.py
@@ -1,32 +1,87 @@
+import google_auth_httplib2
+import httplib2
+import pandas as pd
 import streamlit as st
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import HttpRequest
 from streamlit_timeline import st_timeline
 
-st.set_page_config(page_title="Cerebro Roadmap", page_icon="🏀", layout="centered")
+
+SCOPE = "https://www.googleapis.com/auth/spreadsheets"
+SPREADSHEET_ID = "13VnmnkJt_P-z1RJ6iWh87kIkE9n-zJr3HfpkzB4wkxY"
+SHEET_NAME = "Database"
+GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
 
 
-from utils import gui
+@st.experimental_singleton()
+def connect_to_gsheet():
+    # Create a connection object.
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=[SCOPE],
+    )
 
-# gui.icon("🏀")
+    # Create a new Http() object for every request
+    def build_request(http, *args, **kwargs):
+        new_http = google_auth_httplib2.AuthorizedHttp(
+            credentials, http=httplib2.Http()
+        )
+        return HttpRequest(new_http, *args, **kwargs)
+
+    authorized_http = google_auth_httplib2.AuthorizedHttp(
+        credentials, http=httplib2.Http()
+    )
+    service = build(
+        "sheets",
+        "v4",
+        requestBuilder=build_request,
+        http=authorized_http,
+    )
+    gsheet_connector = service.spreadsheets()
+    return gsheet_connector
 
 
+def get_data(gsheet_connector) -> pd.DataFrame:
+    values = (
+        gsheet_connector.values()
+        .get(
+            spreadsheetId=SPREADSHEET_ID,
+            range=f"{SHEET_NAME}!A:F",
+        )
+        .execute()
+    )
+
+    df = pd.DataFrame(values["values"])
+    df.columns = df.iloc[0]
+    df = df[1:]
+    return df
 
 
-items = [
-    {"id": 1, "content": "2022-10-20", "start": "2022-10-20"},
-    {"id": 2, "content": "2022-10-09", "start": "2022-10-09"},
-    {"id": 3, "content": "2022-10-18", "start": "2022-10-18"},
-    {"id": 4, "content": "2022-10-16", "start": "2022-10-16"},
-    {"id": 5, "content": "2022-10-25", "start": "2022-10-25"},
-    {"id": 6, "content": "2022-10-27", "start": "2022-10-27"},
-]
+def add_row_to_gsheet(gsheet_connector, row) -> None:
+    gsheet_connector.values().append(
+        spreadsheetId=SPREADSHEET_ID,
+        range=f"{SHEET_NAME}!A:F",
+        body=dict(values=row),
+        valueInputOption="USER_ENTERED",
+    ).execute()
 
+    
+    
+gsheet_connector = connect_to_gsheet()
+
+df = get_data(gsheet_connector)
+df1 = df[["Author","Description","Expected Completion Date:"]]
+
+def transform_row(row):
+    return {
+        "id": row["Author"],
+        "content": row["Description"],
+        "start": row["Expected Completion Date:"]
+    }
+
+# Apply the function to each row of the DataFrame
+items = df1.apply(transform_row, axis=1).tolist()
 timeline = st_timeline(items, groups=[], options={}, height="300px")
 st.subheader("Selected item")
 st.write(timeline)
-
-
-# Make sure session state is preserved
-for key in st.session_state:
-    st.session_state[key] = st.session_state[key]
-
-st.title("Page in Progress")
